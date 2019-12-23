1a7fad62914e5613308df19c7cf8be88f6a7f38e
Prashanth Babu T <prashanth.babu@1to1core.com>
2019-12-23 06:40:54 +0000

    Update HashContract.sol

--- HashContract.sol
+++ HashContract.sol
@@ -2,9 +2,12 @@ pragma solidity >=0.4.22 <0.6.0;
 
 contract Hash {
     
-    event HashAdded(string _accountId, string _hashId, string _hashType, string _hash,string _version);
+    uint private _counter = 0;
+    
+    event HashAdded(uint _counter, string _accountId, string _hashId, string _hashType, string _hash,string _version);
     
     function addHashData (string memory _accountId, string memory _hashId, string memory _hashType, string memory _hash, string memory _version) public {
-        emit HashAdded(_accountId, _hashId, _hashType, _hash, _version);
+        _counter++;
+        emit HashAdded(_counter, _accountId, _hashId, _hashType, _hash, _version);
     }
 }
