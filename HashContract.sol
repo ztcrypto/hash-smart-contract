75ed22a614874a3e818a873ca70060bdd421a723
Prashanth Babu T <prashanth.babu@1to1core.com>
2019-12-24 07:24:47 +0000

    Update HashContract.sol

--- HashContract.sol
+++ HashContract.sol
@@ -4,9 +4,9 @@ contract Hash {
     
     uint private _counter = 0;
     
-    event HashAdded(uint _counter, string _accountId, string _hashId, string _hashType, string _hash,string _version);
+    event HashAdded(uint _counter, string _accountId, string _hashId, string _hashType, string _hash, uint _version);
     
-    function addHashData (string memory _accountId, string memory _hashId, string memory _hashType, string memory _hash, string memory _version) public {
+    function addHashData (string memory _accountId, string memory _hashId, string memory _hashType, string memory _hash, uint memory _version) public {
         _counter++;
         emit HashAdded(_counter, _accountId, _hashId, _hashType, _hash, _version);
     }
